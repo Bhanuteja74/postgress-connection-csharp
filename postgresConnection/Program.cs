e621ca51168143e6a8345fd656019e306c1a7d54
Bhanuteja74 <145745009+Bhanuteja74@users.noreply.github.com>
2025-06-07 18:53:18 +0530

    Simple DB connection Data Fetch Data Insert Data Remove

---
+++ postgresConnection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Npgsql;
+
+class Program
+{
+    static void Main()
+    {
+        var connectionString = "Host=localhost;Username=bhanutejakommi;Database=postgres";
+
+        var conn = new NpgsqlConnection(connectionString);
+        conn.Open();
+        
+        var deleteCmd = new NpgsqlCommand("DELETE FROM student WHERE age=21", conn);
+        deleteCmd.ExecuteNonQuery();
+        
+        var insertCmd = new NpgsqlCommand("INSERT INTO student(name, age) VALUES (@name, @age)", conn);
+        insertCmd.Parameters.AddWithValue("name", "Teja");
+        insertCmd.Parameters.AddWithValue("age", 21);
+        insertCmd.ExecuteNonQuery();
+
+
+        var cmd = new NpgsqlCommand("SELECT name,age FROM student", conn);
+        var reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine($"Age: {reader.GetInt32(1)}, Name: {reader.GetString(0)}");
+        }
+        
+        conn.Close();
+    }
+}
